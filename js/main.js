e12370f7e6b55ac7fc76b9585070449bf67c1d95
lucalicata <lucalicata@hotmail.com>
2017-10-29 12:38:42 +0000

    Latest commit

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 
 $(document).ready(function(){
+    $("#menu_vertical").click(function(){
+        $("#myNav").css("height", "100%");
+    });
+});
+
+$(document).ready(function(){
+    $(".closebtn").click(function(){
+        $("#myNav").css("height", "0%");
+    });
+});
+
+$(document).ready(function(){
 
 
 
     $('#fullpage').fullpage({
 
-        anchors :['video-section','first-section','second-section','third-section','fourth-section','fifth-section']
+        anchors :['first-section','second-section','third-section','fourth-section','fifth-section']
 
     });
-
-
-function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-}
-
-function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-
-}
-
-
 });
