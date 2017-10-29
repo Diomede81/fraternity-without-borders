5f8fda4840f3c91306f891117eecc32e75f832e5
TomJamesDuffy <tomjamesduffy@gmail.com>
2017-10-29 12:10:25 +0000

    Fixed merging conflicts.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 $(document).ready(function(){
     $("#menu_vertical").click(function(){
         $("#myNav").css("height", "100%");
@@ -10,7 +10,6 @@ $(document).ready(function(){
         $("#myNav").css("height", "0%");
     });
 });
-=======
 
 $(document).ready(function(){
 
@@ -21,17 +20,4 @@ $(document).ready(function(){
         anchors :['first-section','second-section','third-section','fourth-section','fifth-section']
 
     });
-
-
-function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-}
-
-function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-
-}
-
-
 });
->>>>>>> 78d2d26ee422f0aa44daa232a2d00f6518757e64
